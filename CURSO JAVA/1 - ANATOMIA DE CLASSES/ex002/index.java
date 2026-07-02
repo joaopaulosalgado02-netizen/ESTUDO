0283e359c4b452fabe8e7abeff077f2a35f9d57b
João <joaopaulosalgado02@gmail.com>
2026-07-02 14:57:55 -0300

    Adicionar novos exemplos de classes em Java para estudo

---
+++ CURSO JAVA/1 - ANATOMIA DE CLASSES/ex002/index.java
@@ -0,0 +1,10 @@
+public class index {
+    public static void main(String[] args) {
+
+        int ano = 2024;
+        ano = 2025; 
+        String BR = "Brasil";
+        final String nome = "João";
+
+    }
+}
---
+++ CURSO JAVA/1 - ANATOMIA DE CLASSES/ex003/index.java
@@ -0,0 +1,10 @@
+public class index {
+    public static void main(String[] args) {
+        
+        String meuNome = "João";
+
+        int anoFabricacao = 2022;
+        boolean isAtivo = true;
+
+    }
+}
