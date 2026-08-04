b6465578706b16b97f2a90420872efbd5ec83519
joaopaulosalgado02-netizen <joaopaulosalgado02@gmail.com>
2026-08-03 23:16:20 -0300

    Add Windows runner utilities and main window implementation
    - Created utils.cpp and utils.h for console management and command line argument handling.
    - Implemented win32_window.cpp and win32_window.h for high DPI-aware window management.
    - Added necessary includes and defined window class registration and message handling.
    - Introduced plugin registration and package configuration files for Flutter web support.
    - Configured IDE settings and run configurations for the Flutter project.

---
+++ FORMAÇÃO FLUTTER ANDROID, IOS E APPS WEB/1 - INTRODUÇÃO/ex001/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('Exemplo de AppBar'),
+            backgroundColor: Colors.purple,
+          ),
+          
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            backgroundColor: Colors.purple,
+            child: Icon(Icons.add, size: 30,
+            ) 
+          ),
+        ),
+      ),
+    );
+  }
+}
